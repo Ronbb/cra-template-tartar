81a043d2baa23fd9b206e1b568333d2f43dd94dc
ronbb <ronbiaobiao@gmail.com>
2020-04-26 01:22:47 +0800

    New template display

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,23 +1,37 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-import { Button } from 'antd'
+import './App.less'
+import { Button, Typography, Layout, Space } from 'antd'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img alt="logo" className="App-logo" src={logo} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" rel="noopener noreferrer" target="_blank">
-          Learn React
-        </a>
-        <Button type="primary">111</Button>
-      </header>
-    </div>
-  )
+const { Content } = Layout
+const { Paragraph, Text, Title } = Typography
+
+class App extends React.Component {
+  render() {
+    return (
+      <Layout className="App">
+        <Content className="App-content">
+          <Typography className="tar-tar">
+            <Title>Tartar sauce</Title>
+            <Paragraph>
+              <Text strong>Tartar sauce</Text> (French: <Text underline>sauce tartare</Text>; also spelled{' '}
+              <Text strong>Tartare sauce</Text> in the UK, New Zealand, Australia, South Africa, and other parts of the
+              Commonwealth) is a condiment primarily composed of mayonnaise and finely chopped capers. Tartar sauce can
+              also be enhanced with the addition of gherkins, other varieties of pickles, and lemon juice as well as
+              herbs such as dill and parsley.
+            </Paragraph>
+            <Paragraph>
+              It is most often used as a condiment with seafood dishes such as clam strips, fried oysters, fish and
+              chips, fish sandwiches, and fish fingers, among many other dishes.
+            </Paragraph>
+          </Typography>
+          <Space>
+            <Button type="primary">Have a try</Button>
+            <Button>No interest</Button>
+          </Space>
+        </Content>
+      </Layout>
+    )
+  }
 }
 
 export default App
