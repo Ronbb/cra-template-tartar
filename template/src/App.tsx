c7d397e3af0d7cb7399e90368d996623f0a9f7a1
ronbb <ronbiaobiao@gmail.com>
2020-04-28 17:16:05 +0800

    Modify template

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -13,15 +13,16 @@ class App extends React.Component {
           <Typography className="tar-tar">
             <Title>Tartar sauce</Title>
             <Paragraph>
-              <Text strong>Tartar sauce</Text> (French: <Text underline>sauce tartare</Text>; also spelled{' '}
-              <Text strong>Tartare sauce</Text> in the UK, New Zealand, Australia, South Africa, and other parts of the
-              Commonwealth) is a condiment primarily composed of mayonnaise and finely chopped capers. Tartar sauce can
-              also be enhanced with the addition of gherkins, other varieties of pickles, and lemon juice as well as
-              herbs such as dill and parsley.
+              <Text strong>Tartar sauce</Text>(French: <Text underline>sauce tartare</Text>; also
+              spelled <Text strong>Tartare sauce</Text> in the UK, New Zealand, Australia, South
+              other parts of the Commonwealth) is a condiment primarily composed of mayonnaise and
+              finely chopped capers. Tartar sauce can also be enhanced with the addition of
+              gherkins, other varieties of pickles, and lemon juice as well as herbs such as dill
+              and parsley.
             </Paragraph>
             <Paragraph>
-              It is most often used as a condiment with seafood dishes such as clam strips, fried oysters, fish and
-              chips, fish sandwiches, and fish fingers, among many other dishes.
+              It is most often used as a condiment with seafood dishes such as clam strips, fried
+              oysters, fish and chips, fish sandwiches, and fish fingers, among many other dishes.
             </Paragraph>
           </Typography>
           <Space>
