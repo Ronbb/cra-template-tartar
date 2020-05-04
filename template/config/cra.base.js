e343a80558a4afcc35078b14e832fdcba3ce0af4
ronbb <ronbiaobiao@gmail.com>
2020-05-04 10:20:29 +0800

    modify settings

--- template/config/cra.base.js
+++ template/config/cra.base.js
@@ -6,12 +6,13 @@ const {
   addLessLoader,
   addWebpackPlugin
 } = require('customize-cra')
+const path = require('path')
 const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin')
 const theme = require('./theme')
 
 module.exports = override(
   useBabelRc(),
-  useEslintRc(),
+  useEslintRc(path.resolve(__dirname, '../.eslintrc')),
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
